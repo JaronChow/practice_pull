ff17a6b818e46ee67c1b53a4238bf65ee1773d6b
Winnie Liu <pewinonlyy@gmail.com>
2023-02-22 20:08:51 -0800

    testing winnie's branch

--- practice.js
+++ practice.js
@@ -1,1 +1,13 @@
 const string = "bobfgfdkghfjdk";
+
+git clone git@github.com:JaronChow/practice_pull.git // clone from owner's repo
+git remote -v
+code .
+git branch -m "Adam/initial-test"
+git branch
+Git status
+Git add .
+Git commit -m "testing adam’s branch"
+Git push origin Adam/initial-test
+Git checkout main // go to different main
+Git pull
